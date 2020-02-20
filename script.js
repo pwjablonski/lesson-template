403aae53fe371541e667b7b7ca6c884fb2157c5d
Glitch (hello-webpage) <none>
2020-02-20 04:03:46 +0000

    🐡👆 Checkpoint ./script.js:671814/3634 ./index.html:671814/117

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 Reveal.initialize({
   dependencies: [
     {
-      src: 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0/plugin/markdown/marked.js',
-      condition: function() { return !!document.querySelector( '[data-markdown]' ); }
+      src:
+        "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0/plugin/markdown/marked.js",
+      condition: function() {
+        return !!document.querySelector("[data-markdown]");
+      }
     },
     {
-      src: 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0/plugin/markdown/markdown.js',
-      condition: function() { return !!document.querySelector( '[data-markdown]' ); }
+      src:
+        "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0/plugin/markdown/markdown.js",
+      condition: function() {
+        return !!document.querySelector("[data-markdown]");
+      }
     },
-    { src: 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0/plugin/notes/notes.js', async: true },
-    { src: 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.8.0/plugin/highlight/highlight.min.js', async: true },
+    {
+      src:
+        "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0/plugin/notes/notes.js",
+      async: true,
+    },
+    {
+      src:
+        "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.8.0/plugin/highlight/highlight.min.js",
+      async: true,
+      callback: function() {
+        hljs.initHighlightingOnLoad();
+      }
+    }
   ]
 });
